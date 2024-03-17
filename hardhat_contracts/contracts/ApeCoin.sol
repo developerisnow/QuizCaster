74c359180fcaa0608ab1bf3891423937df5407c4
ielboulo <46560616+ielboulo@users.noreply.github.com>
2024-03-17 07:48:55 +0100

    Update ApeCoin.sol

--- hardhat_contracts/contracts/ApeCoin.sol
+++ hardhat_contracts/contracts/ApeCoin.sol
@@ -8,8 +8,10 @@ contract ApeCoin is ERC20 {
     constructor() ERC20("ApeCoin", "ApeCoin") {
         _mint(msg.sender, 1000 * 10 ** decimals()); // Mint 1000 of ApeCoin tokens | 10^18 
     }
+// Workaround : to approve the transfer of tokens from wallet to the smart contract
+// the best option is to call the "approve" function from Front-End
 
     function approve_(address _addr) public  {
-        approve(_addr, 10 + 1000);
+        approve(_addr, 11111);
     }
 }
