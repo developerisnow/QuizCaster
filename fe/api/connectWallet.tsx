61b10c044cee172e8fe87a38132065b77e7c9756
developerisnow <35399970+developerisnow@users.noreply.github.com>
2024-03-17 03:22:16 +0000

    in progress wallet

--- fe/api/connectWallet.tsx
+++ fe/api/connectWallet.tsx
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers';
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react';
 import { MetaMaskInpageProvider } from "@metamask/providers";
 
 declare global {
@@ -7,8 +7,7 @@ declare global {
     ethereum?:MetaMaskInpageProvider
   }
 }
-// Assuming you have the ABI for your contract
-// Updated to use an interface for ContractArtifact
+
 interface ContractArtifact {
   abi: any;
   contractAddress: string;
@@ -20,15 +19,19 @@ const contractABI = NFTQuizzArtifact.abi;
 const contractAddress = '0xA06B908f35e713a5E731BB9D1e50F3F347124e58'
 
 export default function ConnectWalletAndMint() {
-  useEffect(() => {
-    console.log('Component mounted. Waiting for user to click the mint button.');
-  }, [])
+    const [isMinting, setIsMinting] = useState(false);
+    const [error, setError] = useState('');
+  
+    useEffect(() => {
+      console.log('Component mounted. Waiting for user to click the mint button.');
+    }, []);
 
   const connectWalletHandler = async () => {
     console.log('Attempting to connect wallet...');
     if (typeof window.ethereum !== 'undefined') {
       console.log('Ethereum object found in window.');
       try {
+        setIsMinting(true);
         const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
         console.log('Accounts found:', accounts);
 
@@ -46,20 +49,27 @@ export default function ConnectWalletAndMint() {
         console.log('Minting transaction sent:', tx);
 
         console.log('Waiting for the transaction to be mined...');
-        await tx.wait()
-        console.log('Minted -- Transaction:', tx.hash)
+        await tx.wait();
+        console.log('Minted -- Transaction:', tx.hash);
       } catch (err) {
-        console.error('Error during minting process:', err)
+        console.error('Error during minting process:', err);
+        setError(err.message);
+      } finally {
+        setIsMinting(false);
       }
     } else {
       console.log('Ethereum object does not exist! Make sure you have MetaMask installed.');
     }
-  }
+  };
 
   return (
     <div>
-      <button onClick={connectWalletHandler}>Connect Wallet & Mint</button>
-      <p>Connect your wallet to mint a NFT</p>
+      <div onClick={connectWalletHandler} disabled={isMinting}>
+        {isMinting ? 'Minting...' : 'Connect Wallet & Mint'}
+      </div>
+      {/* {error && <p>Error: {error}</p>} */}
+      <p>Connect your wallet to mint an NFT</p>
     </div>
-  )
+  );
 }
+
