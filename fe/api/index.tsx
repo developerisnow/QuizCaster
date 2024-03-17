8d05ff7f26061bfcd6cd07428c4ddb0b88d3bc9e
developerisnow <35399970+developerisnow@users.noreply.github.com>
2024-03-17 04:39:15 +0000

    mint

--- fe/api/index.tsx
+++ fe/api/index.tsx
@@ -122,30 +122,33 @@ app.frame('/beforeEnd', (c) => {
     imageAspectRatio: '1:1',
     intents: [
       <Button>Proceed to Connect Wallet</Button>,
+      <Button.Link href="/connectWallet">Proceed to Connect Wallet</Button.Link>,
     ],
   })
 })
 
-app.frame('/connectWallet', (c) => {
-  return c.res({
-    action: '/mintNFT',
-    image: `/connectWallet.png`,
-    imageAspectRatio: '1:1',
-    intents: [
-      <Button>Connect Wallet</Button>,
-    ],
-  })
-})
+// app.frame('/connectWallet', (c) => {
+//   return c.res({
+//     action: '/mintNFT',
+//     image: `/connectWallet.png`,
+//     imageAspectRatio: '1:1',
+//     intents: [
+//       <Button>Connect Wallet</Button>,
+//       <Button.Link href="https://google.com">Google</Button.Link>,
+//     ],
+//   })
+// })
 
-app.transaction('/mintNFT', (c) => {
-  return c.contract({
-    abi: NFTQuizzAbi,
-    chainId: "eip155:10", // Assuming Base Sepolia 84532
-    functionName: 'safeMint',
-    to: '0xA06B908f35e713a5E731BB9D1e50F3F347124e58',
-    args: ['0x92fB257891a69FBb600Dc7e79EA4A4541254a200']
-  })
-})
+// app.transaction('/mintNFT', (c) => {
+//   return c.contract({
+//     abi: NFTQuizzAbi,
+//     chainId: "eip155:84532", // Assuming Base Sepolia 84532
+//     chainId: "84532",
+//     functionName: 'safeMint',
+//     to: '0xA06B908f35e713a5E731BB9D1e50F3F347124e58',
+//     args: ['0x92fB257891a69FBb600Dc7e79EA4A4541254a200']
+//   })
+// })
 
 app.frame('/end', (c) => {
   const { buttonValue } = c
